2bb9712f5658ea19ea9b9b7322d44456c186f006
Patrick Judd <patrick.judd@hotmail.com>
2016-05-30 18:18:30 -0400

    fixed run.sh

--- launch/run.sh
+++ launch/run.sh
@@ -18,12 +18,3 @@ time .skel/caffe.bin test \
   -iterations $3 
 
 .skel/postprocess.sh
-CAFFE_DIR=/localhome/juddpatr/caffe
-
-ln -s $CAFFE_DIR/examples
-ln -s .skel/libcaffe.so
-
-time .skel/caffe.bin test \
-  -model model.prototxt \
-  -weights .skel/weights.caffemodel \
-  -iterations $1
