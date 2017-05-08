8b87a5d53608e71d27d3e744e15ad4b7557d910d
Patrick Judd <patrick.judd@mail.utoronto.ca>
2017-05-08 13:05:53 -0400

    added test script. added h5 suffix to train_full.sh

---
+++ test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+iters=1
+gpu="-gpu 0"
+
+showhelp(){
+echo "
+Usage: ./test.sh models/<net>
+
+Options:
+  -i        iterations, default=1
+  -c        cpu mode, default=false
+"
+}
+
+while getopts "h?i:c" opt; do
+  case "$opt" in
+  h|\?) showhelp; exit 0 ;;
+  i)    iters=$OPTARG ;;
+  c)    gpu="";;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+
+MODEL_DIR=$1
+
+if [ "$MODEL_DIR" = "" ]; then
+  showhelp
+  exit
+fi
+
+./build/tools/caffe test $gpu -model $MODEL_DIR/train_val.prototxt -weights $MODEL_DIR/weights.caffemodel -iterations $iters
